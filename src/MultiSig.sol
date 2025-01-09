1e8fa376ef53d0aedd390675d75d13482a1edf32
teyik0 <teyik0.fr@icloud.com>
2025-01-09 11:28:27 +0100

    main impl done

---
+++ src/MultiSig.sol
@@ -0,0 +1,346 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+error NotSigner();
+error TxDoesNotExist(uint256 txIndex, uint256 transactionsLength);
+error TxAlreadyExecuted(uint256 txIndex);
+error TxAlreadyConfirmed(uint256 txIndex, address signer);
+
+error SignersRequired();
+error InvalidNumConfirmationsRequired(uint256 provided, uint256 totalSigners);
+error InvalidSigner(address signer);
+error DuplicateSigner(address signer);
+
+error NotEnoughConfirmations(
+    uint256 numConfirmations,
+    uint256 requiredConfirmations
+);
+error TxExecutionFailed();
+
+error TxNotConfirmed(uint256 txIndex, address signer);
+
+error AlreadyASigner(address signer);
+error MaxSignersExceeded();
+
+contract MultiSig {
+    event Deposit(address indexed sender, uint amount, uint balance);
+    event SubmitTransaction(
+        address indexed signer,
+        uint indexed txIndex,
+        address indexed to,
+        uint value,
+        bytes data
+    );
+    event ConfirmTransaction(address indexed signer, uint indexed txIndex);
+    event RevokeConfirmation(address indexed signer, uint indexed txIndex);
+    event ExecuteTransaction(address indexed signer, uint indexed txIndex);
+
+    address[] public signers;
+
+    mapping(address => bool) public isSigner;
+
+    uint public numConfirmationsRequired;
+    struct Transaction {
+        address to;
+        uint value;
+        bytes data;
+        bool executed;
+        uint numConfirmations;
+    }
+
+    // mapping from tx index => signer => bool
+    // this verify if a tx has been confirmed by a signer
+    mapping(uint => mapping(address => bool)) public isConfirmed;
+
+    Transaction[] public transactions;
+
+    modifier onlySigner() {
+        if (!isSigner[msg.sender]) {
+            revert NotSigner();
+        }
+        _;
+    }
+
+    modifier txExists(uint _txIndex) {
+        if (_txIndex >= transactions.length) {
+            revert TxDoesNotExist(_txIndex, transactions.length);
+        }
+        _;
+    }
+
+    modifier notExecuted(uint _txIndex) {
+        if (transactions[_txIndex].executed) {
+            revert TxAlreadyExecuted(_txIndex);
+        }
+        _;
+    }
+
+    modifier notConfirmed(uint _txIndex) {
+        if (isConfirmed[_txIndex][msg.sender]) {
+            revert TxAlreadyConfirmed(_txIndex, msg.sender);
+        }
+        _;
+    }
+
+    constructor(address[] memory _signers, uint _numConfirmationRequired) {
+        if (_signers.length < 2) {
+            // 2 signers minimum
+            revert SignersRequired();
+        }
+
+        if (
+            _numConfirmationRequired < 2 || // 2 signers minimum on each tx
+            _numConfirmationRequired > _signers.length
+        ) {
+            revert InvalidNumConfirmationsRequired(
+                _numConfirmationRequired,
+                _signers.length
+            );
+        }
+
+        for (uint i = 0; i < _signers.length; i++) {
+            address signer = _signers[i];
+            if (signer == address(0)) {
+                revert InvalidSigner(signer);
+            }
+            if (isSigner[signer]) {
+                revert DuplicateSigner(signer);
+            }
+            isSigner[signer] = true;
+            signers.push(signer);
+        }
+
+        numConfirmationsRequired = _numConfirmationRequired;
+    }
+
+    function submitTransaction(
+        address _to,
+        uint _value,
+        bytes memory _data
+    ) public onlySigner {
+        uint txIndex = transactions.length;
+        transactions.push(
+            Transaction({
+                to: _to,
+                value: _value,
+                data: _data,
+                executed: false,
+                numConfirmations: 1 // 1 correspond to the submitter of the tx
+            })
+        );
+        isConfirmed[txIndex][msg.sender] = true;
+        emit SubmitTransaction(msg.sender, txIndex, _to, _value, _data);
+    }
+
+    function confirmTransaction(
+        uint _txIndex
+    )
+        public
+        onlySigner
+        txExists(_txIndex)
+        notExecuted(_txIndex)
+        notConfirmed(_txIndex)
+    {
+        Transaction storage transaction = transactions[_txIndex];
+        transaction.numConfirmations += 1;
+        isConfirmed[_txIndex][msg.sender] = true;
+        emit ConfirmTransaction(msg.sender, _txIndex);
+
+        if (transaction.numConfirmations >= numConfirmationsRequired) {
+            _executeTransaction(_txIndex);
+        }
+    }
+
+    function _executeTransaction(uint _txIndex) private {
+        Transaction storage transaction = transactions[_txIndex];
+        transaction.executed = true;
+
+        (bool success, ) = transaction.to.call{value: transaction.value}(
+            transaction.data
+        );
+        if (!success) {
+            revert TxExecutionFailed();
+        }
+        emit ExecuteTransaction(msg.sender, _txIndex);
+    }
+
+    function revokeConfirmation(
+        uint _txIndex
+    ) public onlySigner txExists(_txIndex) notExecuted(_txIndex) {
+        Transaction storage transaction = transactions[_txIndex];
+
+        if (!isConfirmed[_txIndex][msg.sender]) {
+            revert TxNotConfirmed(_txIndex, msg.sender);
+        }
+
+        transaction.numConfirmations -= 1;
+        isConfirmed[_txIndex][msg.sender] = false;
+
+        emit RevokeConfirmation(msg.sender, _txIndex);
+    }
+
+    function getSigners() public view returns (address[] memory) {
+        return signers;
+    }
+
+    function getTransactionCount() public view returns (uint) {
+        return transactions.length;
+    }
+
+    function getTransaction(
+        uint _txIndex
+    )
+        public
+        view
+        returns (
+            address to,
+            uint value,
+            bytes memory data,
+            bool executed,
+            uint numConfirmations
+        )
+    {
+        Transaction storage transaction = transactions[_txIndex];
+        return (
+            transaction.to,
+            transaction.value,
+            transaction.data,
+            transaction.executed,
+            transaction.numConfirmations
+        );
+    }
+
+    event AddSignerRequestSubmitted(uint indexed requestId, address newSigner);
+    event AddSignerRequestConfirmed(
+        address indexed signer,
+        uint indexed requestId
+    );
+    event AddSignerRequestExecuted(address indexed newSigner);
+    event RevokeSignerRequestExecuted(address indexed newSigner);
+
+    // mapping from tx new signer index => signer => bool
+    // this verify if a tx new singer has been confirmed by a signer
+    mapping(uint => mapping(address => bool)) public isNewSignerConfirmed;
+
+    struct SignerRequest {
+        address newSigner;
+        uint numConfirmations;
+        bool executed;
+        bool addOrRevoke; // true pour ajouter, false pour retirer
+    }
+    SignerRequest[] public signerRequests;
+
+    function submitSignerRequest(
+        address _newSigner,
+        bool addOrRevoke
+    ) public onlySigner {
+        if (_newSigner == address(0)) {
+            revert InvalidSigner(_newSigner);
+        }
+
+        uint requestId = signerRequests.length;
+        if (addOrRevoke) {
+            if (isSigner[_newSigner]) {
+                revert AlreadyASigner(_newSigner);
+            }
+            signerRequests.push(
+                SignerRequest({
+                    newSigner: _newSigner,
+                    numConfirmations: 1,
+                    executed: false,
+                    addOrRevoke: true
+                })
+            );
+        } else {
+            if (!isSigner[_newSigner] && !addOrRevoke) {
+                revert InvalidSigner(_newSigner);
+            }
+            if (signers.length == 2) {
+                revert("Cannot remove signer, minimum 2 signers required");
+            }
+            signerRequests.push(
+                SignerRequest({
+                    newSigner: _newSigner,
+                    numConfirmations: 1,
+                    executed: false,
+                    addOrRevoke: false
+                })
+            );
+        }
+
+        emit AddSignerRequestSubmitted(requestId, _newSigner);
+    }
+
+    function confirmSignerRequest(uint _requestId) public onlySigner {
+        if (_requestId >= signerRequests.length) {
+            revert TxDoesNotExist(_requestId, signerRequests.length);
+        }
+
+        SignerRequest storage request = signerRequests[_requestId];
+
+        if (request.executed) {
+            revert TxAlreadyExecuted(_requestId);
+        }
+        if (isNewSignerConfirmed[_requestId][msg.sender]) {
+            revert TxAlreadyConfirmed(_requestId, msg.sender);
+        }
+
+        isNewSignerConfirmed[_requestId][msg.sender] = true;
+        request.numConfirmations += 1;
+
+        emit AddSignerRequestConfirmed(msg.sender, _requestId);
+
+        if (request.numConfirmations >= numConfirmationsRequired) {
+            if (request.addOrRevoke) {
+                _executeAddSignerRequest(request);
+            } else {
+                _executeRevokeSignerRequest(request);
+            }
+        }
+    }
+
+    function _executeAddSignerRequest(SignerRequest storage request) private {
+        isSigner[request.newSigner] = true;
+        signers.push(request.newSigner);
+
+        request.executed = true;
+
+        emit AddSignerRequestExecuted(request.newSigner);
+    }
+
+    function _executeRevokeSignerRequest(SignerRequest memory request) private {
+        uint signerIndex = signerIndexOf(request.newSigner);
+
+        signers[signerIndex] = signers[signers.length - 1];
+        signers.pop();
+
+        isSigner[request.newSigner] = false;
+
+        request.executed = true;
+
+        emit RevokeSignerRequestExecuted(request.newSigner);
+    }
+
+    function signerIndexOf(address _signer) internal view returns (uint) {
+        for (uint i = 0; i < signers.length; i++) {
+            if (signers[i] == _signer) {
+                return i;
+            }
+        }
+        revert("Signer not found");
+    }
+
+    function getSignerRequestsCount() public view returns (uint) {
+        return signerRequests.length;
+    }
+
+    function getSignerRequest(
+        uint _requestId
+    ) public view returns (SignerRequest memory) {
+        return signerRequests[_requestId];
+    }
+
+    receive() external payable {
+        emit Deposit(msg.sender, msg.value, address(this).balance);
+    }
+}
---
+++ test/MultiSig.t.sol
@@ -0,0 +1,358 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MultiSig} from "../src/MultiSig.sol";
+
+// forge coverage --report debug > report.log
+contract MultiSigTest is Test {
+    MultiSig public multiSig;
+
+    address public constant USER1 = address(0x1);
+    address public constant USER2 = address(0x2);
+    address public constant USER3 = address(0x3);
+    address public constant USER4 = address(0x4);
+    address public constant USER5 = address(0x5);
+    address public constant USER6 = address(0x6);
+
+    function toDynamicArr(
+        address[4] memory staticArray
+    ) internal pure returns (address[] memory) {
+        address[] memory dynamicArray = new address[](4);
+        for (uint i = 0; i < 4; i++) {
+            dynamicArray[i] = staticArray[i];
+        }
+        return dynamicArray;
+    }
+
+    function setUp() public {
+        address[4] memory staticArray = [USER1, USER2, USER3, USER4];
+        multiSig = new MultiSig(toDynamicArr(staticArray), 3);
+
+        vm.deal(address(multiSig), 10 ether);
+
+        vm.deal(USER1, 1 ether);
+        vm.deal(USER2, 1 ether);
+        vm.deal(USER3, 1 ether);
+        vm.deal(USER4, 1 ether);
+        vm.deal(USER5, 1 ether);
+        vm.deal(USER6, 1 ether);
+    }
+
+    function test_submitTransaction() public {
+        address to = address(0x4);
+        uint value = 1 ether;
+        bytes memory data = "0x";
+
+        vm.startPrank(USER1);
+        multiSig.submitTransaction(to, value, data);
+
+        (
+            address transactionTo,
+            uint transactionValue,
+            bytes memory transactionData,
+            bool executed,
+            uint numConfirmations
+        ) = multiSig.getTransaction(0);
+
+        assertEq(transactionTo, to, "Transaction 'to' address mismatch");
+        assertEq(transactionValue, value, "Transaction value mismatch");
+        assertEq(transactionData, data, "Transaction data mismatch");
+        assertEq(executed, false, "Transaction should not be executed");
+        assertEq(
+            numConfirmations,
+            1,
+            "Transaction should have 1 confirmation initially"
+        );
+        vm.stopPrank();
+    }
+
+    function test_submitTransaction_failed_notASigner() public {
+        address to = address(0x247);
+        uint value = 1 ether;
+        bytes memory data = "0x";
+
+        vm.startPrank(USER5);
+        vm.expectRevert(abi.encodeWithSignature("NotSigner()"));
+        multiSig.submitTransaction(to, value, data);
+        vm.stopPrank();
+    }
+
+    function test_confirmTransaction() public {
+        address to = address(0x247);
+        uint value = 1 ether;
+        bytes memory data = "0x";
+
+        vm.startPrank(USER1);
+        multiSig.submitTransaction(to, value, data);
+        vm.stopPrank();
+
+        vm.startPrank(USER2);
+        multiSig.confirmTransaction(0);
+        vm.stopPrank();
+
+        (, , , bool executed, uint numConfirmations) = multiSig.getTransaction(
+            0
+        );
+        assertEq(
+            numConfirmations,
+            2,
+            "Number of confirmations should be 2 after confirmation"
+        );
+        assertEq(executed, false, "Transaction has not been executed yet");
+    }
+
+    function test_executeTransaction() public {
+        address to = address(0x247);
+        uint value = 1 ether;
+        bytes memory data = "0x";
+
+        vm.startPrank(USER1);
+        multiSig.submitTransaction(to, value, data);
+        vm.stopPrank();
+
+        vm.startPrank(USER2);
+        multiSig.confirmTransaction(0);
+        vm.stopPrank();
+
+        vm.startPrank(USER3);
+        multiSig.confirmTransaction(0);
+        vm.stopPrank();
+
+        (, , , bool executed, uint numConfirmations) = multiSig.getTransaction(
+            0
+        );
+        assertEq(
+            numConfirmations,
+            3,
+            "Number of confirmations should be 3 after confirmation"
+        );
+        assertEq(executed, true, "Transaction has been executed");
+    }
+
+    function test_confirmTransaction_failed_notASigner() public {
+        address to = address(0x247);
+        uint value = 1 ether;
+        bytes memory data = "0x";
+        vm.startPrank(USER1);
+        multiSig.submitTransaction(to, value, data);
+        vm.stopPrank();
+
+        vm.startPrank(USER5);
+        vm.expectRevert(abi.encodeWithSignature("NotSigner()"));
+        multiSig.confirmTransaction(0);
+        vm.stopPrank();
+    }
+
+    function test_confirmTransaction_failed_txNotExist() public {
+        vm.startPrank(USER1);
+        uint txIndex = 999;
+        uint txCount = multiSig.getTransactionCount();
+
+        vm.expectRevert(
+            abi.encodeWithSignature(
+                "TxDoesNotExist(uint256,uint256)",
+                txIndex,
+                txCount
+            )
+        );
+
+        multiSig.confirmTransaction(txIndex);
+        vm.stopPrank();
+    }
+
+    function test_confirmTransaction_failed_txAlreadyExecuted() public {
+        address to = address(0x247);
+        uint value = 1 ether;
+        bytes memory data = "0x";
+
+        vm.startPrank(USER1);
+        multiSig.submitTransaction(to, value, data);
+        vm.stopPrank();
+
+        vm.startPrank(USER2);
+        multiSig.confirmTransaction(0);
+        vm.stopPrank();
+
+        vm.startPrank(USER3);
+        multiSig.confirmTransaction(0);
+        vm.stopPrank();
+
+        vm.startPrank(USER4);
+        vm.expectRevert(
+            abi.encodeWithSignature("TxAlreadyExecuted(uint256)", 0)
+        );
+        multiSig.confirmTransaction(0);
+        vm.stopPrank();
+    }
+
+    function test_confirmTransaction_failed_alreadyConfirmed() public {
+        vm.startPrank(USER1);
+        multiSig.submitTransaction(address(0x123), 100, "data");
+        vm.expectRevert(
+            abi.encodeWithSignature(
+                "TxAlreadyConfirmed(uint256,address)",
+                0,
+                USER1
+            )
+        );
+        multiSig.confirmTransaction(0);
+        vm.stopPrank();
+    }
+
+    function test_revokeConfirmation() public {
+        vm.startPrank(USER1);
+        multiSig.submitTransaction(address(0x123), 100, "data");
+        multiSig.revokeConfirmation(0);
+
+        (
+            address to,
+            uint value,
+            ,
+            bool executed,
+            uint numConfirmations
+        ) = multiSig.getTransaction(0);
+        assertEq(numConfirmations, 0);
+        assertEq(executed, false);
+        assertEq(to, address(0x123));
+        assertEq(value, 100);
+        vm.stopPrank();
+    }
+
+    function test_revokeConfirmation_failed_notASigner() public {
+        vm.startPrank(USER1);
+        multiSig.submitTransaction(address(0x123), 100, "data");
+        vm.stopPrank();
+
+        vm.startPrank(USER5);
+        uint txIndex = multiSig.getTransactionCount() - 1;
+        vm.expectRevert(abi.encodeWithSignature("NotSigner()"));
+        multiSig.revokeConfirmation(txIndex);
+
+        vm.stopPrank();
+    }
+
+    function test_revokeConfirmation_failed_txNotExist() public {
+        vm.startPrank(USER1);
+        uint nonExistentTxIndex = 999;
+        vm.expectRevert(
+            abi.encodeWithSignature(
+                "TxDoesNotExist(uint256,uint256)",
+                nonExistentTxIndex,
+                multiSig.getTransactionCount()
+            )
+        );
+        multiSig.revokeConfirmation(nonExistentTxIndex);
+        vm.stopPrank();
+    }
+
+    function test_revokeConfirmation_failed_txAlreadyExecuted() public {
+        vm.startPrank(USER1);
+        multiSig.submitTransaction(address(0x123), 100, "data");
+        vm.stopPrank();
+
+        vm.startPrank(USER2);
+        multiSig.confirmTransaction(0);
+        vm.stopPrank();
+
+        vm.startPrank(USER3);
+        multiSig.confirmTransaction(0);
+
+        uint txIndex = multiSig.getTransactionCount() - 1;
+        vm.expectRevert(
+            abi.encodeWithSignature("TxAlreadyExecuted(uint256)", txIndex)
+        );
+        multiSig.revokeConfirmation(txIndex);
+
+        vm.stopPrank();
+    }
+
+    function test_getSigners() public view {
+        address[] memory signers = multiSig.getSigners();
+        assertEq(signers.length, 4);
+        assertEq(signers[0], USER1);
+        assertEq(signers[1], USER2);
+        assertEq(signers[2], USER3);
+        assertEq(signers[3], USER4);
+    }
+
+    function test_getTransactionCount() public {
+        vm.startPrank(USER1);
+        multiSig.submitTransaction(address(0x123), 100, "data");
+        multiSig.submitTransaction(address(0x123), 100, "data");
+        multiSig.submitTransaction(address(0x123), 100, "data");
+        vm.stopPrank();
+
+        assertEq(multiSig.getTransactionCount(), 3);
+    }
+
+    function test_submitAddSignerRequest() public {
+        vm.startPrank(USER1);
+        multiSig.submitSignerRequest(USER5, true);
+        vm.stopPrank();
+        MultiSig.SignerRequest memory request = multiSig.getSignerRequest(0);
+
+        assertEq(multiSig.getSignerRequestsCount(), 1);
+        assertEq(request.newSigner, USER5);
+        assertEq(request.addOrRevoke, true);
+        assertEq(request.executed, false);
+        assertEq(request.numConfirmations, 1);
+    }
+
+    function test_submitAddSignerRequest_failed_notASigner() public {
+        vm.startPrank(USER5);
+        vm.expectRevert(abi.encodeWithSignature("NotSigner()"));
+        multiSig.submitSignerRequest(USER5, true);
+        vm.stopPrank();
+    }
+
+    function test_confirmSignerRequest() public {
+        vm.startPrank(USER1);
+        multiSig.submitSignerRequest(USER5, true);
+        vm.stopPrank();
+
+        vm.startPrank(USER2);
+        multiSig.confirmSignerRequest(0);
+        vm.stopPrank();
+
+        MultiSig.SignerRequest memory request = multiSig.getSignerRequest(0);
+        assertEq(request.numConfirmations, 2);
+        assertEq(request.executed, false);
+        assertEq(request.newSigner, USER5);
+    }
+
+    function test_executeSignerRequest() public {
+        vm.startPrank(USER1);
+        multiSig.submitSignerRequest(USER5, true);
+        vm.stopPrank();
+
+        vm.startPrank(USER2);
+        multiSig.confirmSignerRequest(0);
+        vm.stopPrank();
+
+        vm.startPrank(USER3);
+        multiSig.confirmSignerRequest(0);
+        vm.stopPrank();
+
+        MultiSig.SignerRequest memory request = multiSig.getSignerRequest(0);
+        address[] memory signers = multiSig.getSigners();
+        assertEq(request.numConfirmations, 3);
+        assertEq(request.executed, true);
+        assertEq(request.newSigner, USER5);
+        assertEq(signers.length, 5);
+    }
+
+    function test_executeRevokeSignerRequest() public {
+        vm.startPrank(USER1);
+        multiSig.submitSignerRequest(USER4, false);
+        vm.stopPrank();
+
+        vm.startPrank(USER2);
+        multiSig.confirmSignerRequest(0);
+        vm.stopPrank();
+
+        vm.startPrank(USER3);
+        multiSig.confirmSignerRequest(0);
+        vm.stopPrank();
+    }
+}
