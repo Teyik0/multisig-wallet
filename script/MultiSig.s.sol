67fe9580bfedfe4edfa885d5ef346dedd656f657
teyik0 <teyik0.fr@icloud.com>
2025-01-09 12:28:56 +0100

    add script deployment

--- script/MultiSig.s.sol
+++ script/MultiSig.s.sol
@@ -9,10 +9,24 @@ contract CounterScript is Script {
 
     function setUp() public {}
 
+    function toDynamicArr(
+        address[3] memory staticArray
+    ) internal pure returns (address[] memory) {
+        address[] memory dynamicArray = new address[](4);
+        for (uint i = 0; i < 3; i++) {
+            dynamicArray[i] = staticArray[i];
+        }
+        return dynamicArray;
+    }
+
     function run() public {
         vm.startBroadcast();
-
-        // multiSig = new MultiSig();
+        address[3] memory signers = [
+            vm.envAddress("ADDRESS1"),
+            vm.envAddress("ADDRESS2"),
+            vm.envAddress("ADDRESS3")
+        ];
+        multiSig = new MultiSig(toDynamicArr(signers), 2);
 
         vm.stopBroadcast();
     }
